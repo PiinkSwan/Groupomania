e29b24b65e1cc40487574e9dd4dedc138a9fef71
Marlier Whitney <marlier.whitney@outlook.com>
2022-07-23 16:59:52 +0200

    update frontend

---
+++ groupomania/src/components/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import '../components/Header'
+
+function Header() {
+  return <Header />
+
+}
+
+export default Header;
---
+++ groupomania/src/components/Header.js
@@ -0,0 +1,5 @@
+function Header () {
+    return <h1>Accueil</h1>
+}
+
+export default Header 
